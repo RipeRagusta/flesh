f91ae877fde5ddf190d394e4868e3642dc4d5bb0
RipeRagusta <132228468+RipeRagusta@users.noreply.github.com>
2025-12-18 04:22:34 -0600

    Add files via upload

--- randomizer.js
+++ randomizer.js
@@ -11,7 +11,8 @@ const pageIds = [
 				  [8, "./8.html"],
 				  [9, "./9.html"],
 				  [10, "./10.html"],
-				  [11, "./11.html"]
+				  [11, "./11.html"],
+				  [12, "./12.html"]
 				];
 
 const indexFlipped = new Set([1, 2]);
