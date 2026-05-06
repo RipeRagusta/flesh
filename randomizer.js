7a935dce6e5969e065fa53d7344d98ca4caa715d
RipeRagusta <132228468+RipeRagusta@users.noreply.github.com>
2026-05-05 20:01:47 -0500

    Add files via upload

--- randomizer.js
+++ randomizer.js
@@ -6,12 +6,13 @@ const pageIds = [
 				  [3, "./3.html"],
 				  [4, "./4.html"],
 				  [5, "./5.html"],
-				  [6, "./6.html"]
+				  [6, "./6.html"],
+				  [7, "./7.html"]
 				];
 
 const pinkBackground = new Set([1, 5, 6]);
 const whiteBackground = new Set([4]);
-const yellowBackground = new Set([2]);
+const yellowBackground = new Set([2, 7]);
 const blackBackground = new Set([3]);
 
 const rare = new Set([]);
